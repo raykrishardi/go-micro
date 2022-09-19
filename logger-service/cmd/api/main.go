9c385cbcf01f52837afeff68d3e6260b4dd6737e
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-19 20:30:04 +0700

    Section 4: Building a logger service 28. Getting started with the logger service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// Create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// Connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("error connecting to mongo:", err)
+		return nil, err
+	}
+
+	return c, nil
+}
