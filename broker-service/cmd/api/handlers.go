5d3e4fa4409c2290b52d3a062b1e183165cfb2d9
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-20 20:18:43 +0700

    Section 4: Building a logger service 34. Adding a route and handler on the broker to communicate with the logger service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +44,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		if err != nil {
 			app.errorJSON(w, errors.New("unknown action"))
@@ -60,6 +68,8 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		app.errorJSON(w, err)
 		return
 	}
+
+	request.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
@@ -90,6 +100,54 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 
 	if jsonFromService.Error {
 		app.errorJSON(w, errors.New("get error response from the auth service"), http.StatusUnauthorized)
+		return
+	}
+
+	app.writeJSON(w, http.StatusAccepted, jsonFromService)
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	// Marshall/convert to json the payload
+	jsonData, err := json.MarshalIndent(&entry, "", "\t")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	// Call the logger microservice
+	request, err := http.NewRequest("POST", "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		// Something went wrong on the server side
+		app.errorJSON(w, errors.New("error calling logger service"))
+		return
+	}
+
+	// create a variable we'll read response.Body into
+	jsonFromService := jsonResponse{}
+
+	// decode/unmarshal the json from the auth service (basically this is an alternative to UNMARSHALLING)
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if jsonFromService.Error {
+		app.errorJSON(w, errors.New("get error response from the logger service"), http.StatusUnauthorized)
 		return
 	}
 
