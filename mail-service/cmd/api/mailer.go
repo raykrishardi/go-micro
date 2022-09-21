445bb205037c03168e67cbf476a0aee489d64b98
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-21 22:02:13 +0700

    Section 5: Building a mail service 41. Building the login to send email

---
+++ mail-service/cmd/api/mailer.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"time"
+
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+type Mail struct {
+	Domain      string // What domain are we using to send mail?
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string // Default from address if Message.From contains no value
+	FromName    string // Default from name if Message.FromName contains no value
+}
+
+type Message struct {
+	From        string // Email address
+	FromName    string // Person's name of that email address
+	To          string
+	Subject     string
+	Attachments []string // Slice of path name of files that want to be attached
+	Data        interface{}
+	DataMap     map[string]interface{}
+}
+
+func (m *Mail) SendSMTPMessage(msg Message) error {
+	if msg.From == "" {
+		msg.From = m.FromAddress
+	}
+
+	if msg.FromName == "" {
+		msg.FromName = m.FromName
+	}
+
+	data := map[string]interface{}{
+		"message": msg.Data,
+	}
+
+	msg.DataMap = data
+
+	// HTML message
+	formattedMessage, err := m.buildHTMLMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	plainMessage, err := m.buildPlainTextMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	// Setup the mail server info
+	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	server.Encryption = m.getEncryption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	smtpClient, err := server.Connect()
+	if err != nil {
+		return err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.From).
+		AddTo(msg.To).
+		SetSubject(msg.Subject)
+
+	email.SetBody(mail.TextPlain, plainMessage)
+	email.AddAlternative(mail.TextHTML, formattedMessage)
+
+	if len(msg.Attachments) > 0 {
+		// Full path to the attachment file
+		for _, x := range msg.Attachments {
+			email.AddAttachment(x)
+		}
+	}
+
+	err = email.Send(smtpClient)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WITH CSS STYLING
+func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
+	// Path to our golang template file, use .gohtml extension to work with our extension
+	templateToRender := "./templates/mail.html.gohtml"
+
+	// Give the template a random name and parse the template
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	// Execute template and passing in some data
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	// Format the string to a CSS that looks good for email
+	formattedMessage := tpl.String()
+	formattedMessage, err = m.inlineCSS(formattedMessage)
+	if err != nil {
+		return "", err
+	}
+
+	return formattedMessage, nil
+}
+
+// NO CSS STYLING
+func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
+	templateToRender := "./templates/mail.plain.gohtml"
+
+	t, err := template.New("email-plain").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+
+	return plainMessage, nil
+}
+
+// Add good looking css class to the email
+func (m *Mail) inlineCSS(s string) (string, error) {
+	// These are good default values
+	options := premailer.Options{
+		RemoveClasses:     false, // Don't remove classes
+		CssToAttributes:   false,
+		KeepBangImportant: true, // If they use !important keyword
+	}
+
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		return "", err
+	}
+
+	// This is our final HTML
+	html, err := prem.Transform()
+	if err != nil {
+		return "", err
+	}
+
+	return html, nil
+}
+
+func (m *Mail) getEncryption(s string) mail.Encryption {
+	switch s {
+	case "tls":
+		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return mail.EncryptionSSLTLS
+	case "none", "":
+		return mail.EncryptionNone
+	default:
+		return mail.EncryptionSTARTTLS
+	}
+}
