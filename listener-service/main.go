68d950b0454364f7b4ddac2cd577e3ca1dcad7ed
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-22 22:17:56 +0700

    Section 6: Building a listener service: AMQP with RabbitMQ 49. Creating a stub listener service

---
+++ listener-service/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// try to connect to rabbitmq
+
+	// start listening for messages (subscribe to the topic for event based consumption instead of polling)
+
+	// create consumer
+
+	// watch consumer and consume events/topics
+}
