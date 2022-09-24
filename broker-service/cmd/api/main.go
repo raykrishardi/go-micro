8d8d347278c56f1c85051e561717337d85421103
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-24 15:13:53 +0700

    Section 6: Building a listener service: AMQP with RabbitMQ 57. updating the broker to interact with rabbitmq

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,27 +3,73 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const webPort = "80"
 
-type Config struct {}
+type Config struct {
+	Rabbit *amqp.Connection
+}
 
 func main() {
-	app := Config{}
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer rabbitConn.Close()
+
+	app := Config{
+		Rabbit: rabbitConn,
+	}
 
 	log.Printf("Starting broker service on port %s\n", webPort)
 
 	// define http server
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
 	// start the server
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		// TODO: change to docker service name
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			log.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = c
+			log.Println("connected to rabbitmq")
+			break
+		}
+
+		if counts > 5 {
+			log.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		// continue // optional
+	}
+
+	return connection, nil
 }
